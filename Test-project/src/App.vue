aa4e8d408ba829d28f8407378838e85db351f7ea
WidestSkate <20zamers-d@rondearingutc.com>
2023-07-25 23:52:51 +0100

    initial commit and setup

---
+++ Test-project/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <!-- Navigation Bar -->
+  <NavBar></NavBar>
+
+  <!-- Invoices Goes Here -->
+  <div class="flex text-white mx-20 my-10 p-5 text-2xl rounded-t-lg" style="background: #1f2e42">
+    <h2>Invoice Overview</h2>
+  </div>
+
+  <!-- Actions Goes Here -->
+  <div class="flex text-white mx-20 my-10 p-5 text-2xl rounded-t-lg" style="background: #1f2e42">
+    <h2>Actions</h2>
+  </div>
+  
+  <!-- Map Goes Here -->
+  <div class="flex text-white mx-20 my-10 p-5 text-2xl rounded-t-lg" style="background: #1f2e42">
+    <h2>Project Distribution Map</h2>
+  </div>
+
+  <!-- Bulletin Board Goes Here -->
+  <div class="flex text-white mx-20 my-10 p-5 text-2xl rounded-t-lg" style="background: #1f2e42">
+    <h2>Bulletin Board</h2>
+  </div>
+  
+  <!-- Footer -->
+</template>
+  
+<script>
+
+import NavBar from './components/navBar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    NavBar
+  }
+  
+}
+</script>
+<style>
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+  font-family: 'filson-pro', sans-serif;
+}
+</style>
+  
---
+++ Test-project/src/components/navBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav class="bg-white drop-shadow-md " style="font-family: 'filson-pro', sans-serif;">
+        <div class="max-w-screen-xl flex flex-wrap justify-between mx-20 p-4">
+            <a href="https://flowbite.com/" class="flex items-center">
+                <img src="https://loop.org.uk/wp-content/themes/loop/images/Loop_logo_blue_with_tag.svg" width="170"/>
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-black mx-8" style="color: #1f2d42; font-weight: 1000">Dashboard</span>
+            </a>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'navBar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
